f6a5f036947b5e9e9766e1ab8555c8f695b42ca2
Brantley Jenkins <bcj@email.uscupstate.edu>
2017-03-14 20:03:19 +0000

    finished pages+routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/cpus'
@@ -10,8 +13,6 @@ Rails.application.routes.draw do
   get 'static_pages/storage'
   
   get 'static_pages/cart'
-
-  root 'application#hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "PC Parts Emporium"
+  end
+
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get cpus" do
     get static_pages_cpus_url
     assert_response :success
+    assert_select "title", "CPUs | #{@base_title}"
   end
 
   test "should get gpus" do
